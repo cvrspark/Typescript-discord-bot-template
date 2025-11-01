c8e5b75732e691af44d7c0e53d54d722b3f6b8ae
cvrspark <swgoverdosed1@gmail.com>
2025-11-01 12:26:49 +0100

    updated deploy

--- src/services/deploy.ts
+++ src/services/deploy.ts
@@ -10,13 +10,14 @@ const interactionFolders = fs.readdirSync(interactionsPath);
 for (const folder of interactionFolders) {
     const commandsPath = path.join(interactionsPath, folder, 'commands');
     if (!fs.existsSync(commandsPath)) continue;
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath).default;
-        if ('data' in command && 'execute' in command) {
-            commands.push(command.data.toJSON());
-        }
+        
+        ('data' in command && 'execute' in command)
+            ? commands.push(command.data.toJSON())
+            : console.warn(`Invalid command: ${filePath}`)   
     }
 }
 
@@ -24,12 +25,12 @@ const rest = new REST({ version: '10' }).setToken(config.token);
 
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
+        console.log('Started deploying application (/) commands.');
         const route = config.private
             ? Routes.applicationGuildCommands(config.clientId, config.guildId)
             : Routes.applicationCommands(config.clientId);
         await rest.put(route, { body: commands });
-        console.log('Successfully reloaded application (/) commands.');
+        console.log('Successfully deployed application (/) commands.');
     } catch (error) {
         console.error(error);
     }
